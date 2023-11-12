aa187525c254931829096e4413fcf042386d095e
ssw0420 <tmddn5645@gmail.com>
2023-11-12 17:06:58 +0900

    프로그램 시작 세팅

---
+++ Java_Team_Project/src/englishword/LoginUI.java
@@ -0,0 +1,29 @@
+package englishword;
+
+import java.awt.*; // 그래픽 처리를 위한 클래스들의 경로명
+import java.awt.event.*; // AWT 이벤트 사용을 위한 경로명
+import javax.swing.*; // 스윙 컴포넌트 클래스들의 경로명
+import javax.swing.event.*; // 스윙 이벤트를 위한 경로명
+
+public class LoginUI extends JFrame {
+	public void ShowUser() {
+		
+	}
+	
+	public void CreateUser() {
+		
+	}
+	
+	public void EditUser() {
+		
+	}
+	
+	public void DeleteUser(JList SelectedUser) {
+		
+	}
+	
+	public void Enter(JList SelectedUser) {
+		
+	}
+	
+}
